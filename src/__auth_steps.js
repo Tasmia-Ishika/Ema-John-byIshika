4795b203c94e427506293c401a0ea06502921ee3
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-04-14 03:10:51 +0600

    signup n login added

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -3,8 +3,12 @@
 * 2. npm install firebase.
 * 3. create firebase.init.js and import getAuth to export auth.
 * 4.Firebase settings > authentication > enable email and Password auth.
-* 5.
-*
+* 5.create login, signup component, setup route
+* 6.attach form field handler and form submit handler
+* 7.npm install --save react-firebase-hooks
+* 8.useCreateUserWithEmailAndPassword from react-firebase-hooks
+* 9. if user is created redirect to the expected page
+* 10. 
 *
 *
 */
